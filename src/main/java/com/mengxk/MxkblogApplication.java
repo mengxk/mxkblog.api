761b108554a25a8eebc8c1d912519386c20b9353
mengxk@yntrust.com <1005682757@qq.com>
2018-07-18 20:55:30 +0800

    mxk的第一个博客

---
+++ src/main/java/com/mengxk/MxkblogApplication.java
@@ -0,0 +1,12 @@
+package com.mengxk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxkblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxkblogApplication.class, args);
+	}
+}
